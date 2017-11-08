a17e5eea6dedbd5f32798131b0424203aee2d67e
Margarita Wraith <Kondratieff@live.ru>
2017-11-08 21:16:33 +0300

    + Конструктор копирования  -Функции сложения и умножения

--- Work/Matrix.h
+++ Work/Matrix.h
@@ -9,16 +9,21 @@ class Matrix
 public:
 	Matrix();
 	Matrix(int n, int m);
+	Matrix(const Matrix& matrix);
 	~Matrix();
+	int GetN() { return n; };
+	int GetM() { return m; };
+	double Get(int i, int j);
+	void Set(int i, int j, double d);
 	bool FillMatrix();
-	Matrix* AddMatrix(Matrix &matr);
-	Matrix* MulMatrix(Matrix &matr);
+	Matrix& AddMatrix(const Matrix &matr);
+	Matrix& MulMatrix(const Matrix &matr);
 	void PrintMatrix();
 
 private:
 
 };
-
+void Fun(Matrix m);
 void CheckMatrix();
 
 #endif
