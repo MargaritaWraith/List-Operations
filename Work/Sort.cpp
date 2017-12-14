bb5bc3ef0f164b2dfd26c2c07495ba9ffa5712f6
Margarita Wraith <Kondratieff@live.ru>
2017-12-14 20:28:33 +0300

    + Сортировка слиянием

--- Work/Sort.cpp
+++ Work/Sort.cpp
@@ -5,27 +5,29 @@
 void CheckSort()
 {
 	int Array[15] = { 42,3,7,8,5,2,6,9,11,13,15,0,6,1,7 };
-	Sort(Array, 0, 14);
+	int TempAr[15] = { 0 };
+	Sort(Array, 0, 14, TempAr);
 	for (int i = 0; i < sizeof(Array) / sizeof(int); i++)
 	{
-		cout << Array[i] << "\t";
+		cout << Array[i] << "   ";
 	}
 }
 
-void Swap(int* arr, int l, int r)
+template <class T> void Swap(T* arr, int l, int r)
 {
 	int t = arr[l];
 	arr[l] = arr[r];
 	arr[r] = t;
 }
-	  
-int SortM(int* arr, int l, int r, int m)
+
+template <class T>
+int SortM(T* arr, int l, int r, int m)
 {
 	while (l<m || r>m)
 	{
 		while (arr[l] < arr[m])
 			l++;
-		while (arr[r] >= arr[m] && r>m)
+		while (arr[r] >= arr[m] && r > m)
 			r--;
 		if (l<m && r>m)
 		{
@@ -34,24 +36,61 @@ int SortM(int* arr, int l, int r, int m)
 			--r;
 		}
 		else if (l < m && m == r)
-			{
-				Swap(arr, l, m);
-				m = l;
-			}
+		{
+			Swap(arr, l, m);
+			m = l;
+		}
 		else if (r > m && l == m)
-			{
-				Swap(arr, r, m);
-				m = r;
-			}
+		{
+			Swap(arr, r, m);
+			m = r;
+		}
 	}
 	return m;
 }
 
-void Sort(int* arr, int l, int r)
+template <class T> void Sort(T* arr, int l, int r)
 {
 	if (l >= r) return;
 	int m = (l + r) / 2;
 	m = SortM(arr, l, r, m);
 	Sort(arr, l, m - 1);
 	Sort(arr, m + 1, r);
+}
+
+void Merge(int* dst, int* trg, int l1, int r1, int l2, int r2)
+{										
+	/*cout << endl<<"-----Merge--start----" << endl;
+	for (int i = l1; i <= r1; i++)	cout << dst[i] << ' ';
+	cout << "    "<<endl;
+	for (int i = l2; i <= r2; i++)	cout << dst[i] << ' ';
+	cout << endl;*/
+
+	int i1 = l1, i2 = l2, j = l1;
+	while (i1 <= r1 && i2 <= r2)
+	{
+		if (dst[i1] < dst[i2])
+		{
+			trg[j] = dst[i1]; j++; i1++;
+		}
+		else
+		{
+			trg[j] = dst[i2]; j++, i2++;
+		}
+	}
+	if (i1 > r1 && i2 <= r2)
+		for (; i2 <= r2; i2++, j++)
+			trg[j] = dst[i2];
+	else		for (; i1 <=r1; i1++, j++)			trg[j] = dst[i1];	for (int i = l1; i <= r2; i++) cout << trg[i] << ' ';//	cout << "---merge--end---" << endl;}
+
+void Sort(int* ar, int l, int r, int* tmp)
+{
+	if (l >= r) return;
+	int m = (l + r) / 2;
+	Sort(ar, l, m, tmp);
+	Sort(ar, m + 1, r, tmp);
+	Merge(ar, tmp, l, m, m + 1, r);
+	for (int i = l; i <= r; i++)
+		ar[i] = tmp[i];
+	return;
 }
--- Work/Sort.h
+++ Work/Sort.h
@@ -4,9 +4,13 @@
 using namespace std;
 void CheckSort();
 
-int SortM(int* arr, int l, int r, int m); //находит итоговое место элемента внутри массива
-void Swap(int* arr, int l, int r);		  //меняет местами границы l и r
-void Sort(int* arr, int l, int r);		 //сортирует на отрезке от l до r
+// Сортировка Quick Sort
+template <class T> int SortM(T* arr, int l, int r, int m); //находит итоговое место элемента внутри массива
+template <class T> void Swap(T* arr, int l, int r);		  //меняет местами границы l и r
+template <class T> void Sort(T* arr, int l, int r);		 //сортирует на отрезке от l до r
 
+// Сортировка слиянием
+void Merge(int* dst, int* trg, int l1, int r1, int l2, int r2);  
+void Sort(int* ar, int l, int r, int* tmp);
 
 #endif
